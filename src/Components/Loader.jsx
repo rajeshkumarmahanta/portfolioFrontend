6e98b28c9df573166d1bb1e87c48f32f49e5ddfc
Rajesh Kumar Mahanta <rajeshkumarmahanta2128@gmail.com>
2024-11-23 20:02:27 +0530

    Update Loader.jsx

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Loader = () => {
   return (
     <>
-    <div className='d-flex align-items-center justify-content-center' style={{height:"100vh",width:"100vw"}}>
+    <div className="container">
+    <div className='d-flex align-items-center justify-content-center' style={{width:"100%",height:"100vh"}}>
         <span className="loader"></span>
+        </div>
         </div>
     </>
   )
